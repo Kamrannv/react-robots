a1bbf75a7350d24ee01e90c94deee98d3a66d60e
Kamrannv <kamranamazov@yahoo.com>
2020-05-27 19:41:58 +0400

    Updates

---
+++ precache-manifest.399a75e813063c3c15713c7ad790f4e2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57b19d3fc9442aedd4556e8cb7fb0140",
+    "url": "/react-robots/index.html"
+  },
+  {
+    "revision": "d1a9a43082258b56e2fa",
+    "url": "/react-robots/static/css/main.81183751.chunk.css"
+  },
+  {
+    "revision": "e947a2efc8f77475de6d",
+    "url": "/react-robots/static/js/2.e6371a5f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-robots/static/js/2.e6371a5f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1a9a43082258b56e2fa",
+    "url": "/react-robots/static/js/main.1412dc79.chunk.js"
+  },
+  {
+    "revision": "68a1d14b053499c55fe9",
+    "url": "/react-robots/static/js/runtime-main.0152ec46.js"
+  }
+]);
